2627fd5082a6ac99d7e212ed76c73adbb7b5bdd3
Aziz231 <s3501099@student.rmit.edu.au>
2017-03-27 21:08:24 +1100

    Add notes
    Adding notes to the driver class

--- src/Driver.java
+++ src/Driver.java
@@ -4,5 +4,17 @@ public class Driver {
 	//scanner
 	Scanner s = new Scanner(System.in);
 	//
+	//loop
+		//1. display menu
+		//1.1 display games
+		//1.1.1 choose game
+		//1.1.2 display menu
+		//1.2 display game's athletes
+		//1.2.1 choose athlete
+		//1.2.2 display menu
+		//1.3 run the game 
+		//1.4 Display results
+		//1.5 Display the 
+		//1.6 break
 
 }
